4a7dceeb980fb189e54db02f177fa32ed792bce8
YanchukV <jantschuk@gmail.com>
2017-05-26 15:30:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import todos from './todos';
+import Header from './components/Header';
+import Todo from './components/Todo';
+
+class App extends React.Component {
+
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            todos: this.props.initialData
+        };
+
+        this.handleStatusChange = this.handleStatusChange.bind(this);
+
+
+    }
+
+    handleStatusChange(id){
+        console.log('onStatusChange', id);
+    }
+
+    render(){
+
+        return(
+            <main>
+                <Header title={this.props.title}/>
+                <section className="todo-list">
+                    {this.state.todos.map(todo =>
+
+                        <Todo key={todo.id}
+                              id={todo.id}
+                              title={todo.title}
+                              completed={todo.completed}
+                              onStatusChange={this.handleStatusChange}
+                        />)
+
+                    }
+                </section>
+            </main>
+        );
+    }
+
+}
+
+
+
+App.propTypes = {
+    title: React.PropTypes.string,
+    initialData: React.PropTypes.arrayOf(React.PropTypes.shape({
+        id: React.PropTypes.number.isRequired,
+        title: React.PropTypes.string.isRequired,
+        completed: React.PropTypes.bool.isRequired
+    })).isRequired
+};
+
+App.defaultProps = {
+    title: 'React Todo'
+};
+
+ReactDOM.render(<App initialData={todos} />, document.getElementById('root'));
